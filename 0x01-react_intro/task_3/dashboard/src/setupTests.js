039cf115a21930883e1e9113c17aa1f35d354ae8
wosohd <wosohd@gmail.com>
2024-01-06 10:26:12 -0800

    React test

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -2,9 +2,4 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-// import '@testing-library/jest-dom';
-
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-17-updated';
-
-Enzyme.configure({ adapter: new Adapter() });
+import '@testing-library/jest-dom';
